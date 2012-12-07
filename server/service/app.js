f0da56958a9928e8e86b451826774e62a8a07f5b
Martin Christmann <martin.christmann@gmail.com>
2012-12-07 14:59:49 +0100

    Improved invalid-parameter detection for app.create

--- server/service/app.js
+++ server/service/app.js
@@ -29,47 +29,97 @@ exports.getAll = function(next){
 /* Create a new app from @appObj and insert it to database 
 	 Returns the new app */
 exports.create = function(appObj, next){
-	if(appObj){
-		var app = new db.App();
-		app.identifier = appObj.identifier;
-		app.maxVersionCode = appObj.maxVersionCode;
-		app.graceInterval = appObj.graceInterval;
-		app.graceRetrys = appObj.graceRetrys;
-		app.validTime = appObj.validTime;
-		app.licenses = appObj.licenses;
+	if(typeof appObj == 'undefined' || appObj == null){
+		next(new Error('Missing parameter appObj'));
+		return;
+	} 
+	
+	var app = new db.App();
 
-		//Create the public private key for signin
-		certificateSv.create(appObj.identifier, function(err, pub, priv, cert){
+	//Identifier
+	if(typeof appObj.identifier != 'string' || appObj.identifier == null ){
+		next(new Error('Missing required parameter appObj.identifier'));
+		return;
+	}
+
+	appObj.identifier = appObj.identifier.trim();
+	if(appObj.identifier.length <= 0){
+		next(new Error('appObj.identifier cannot be an empty string. At least length of 1'));
+		return;
+	}
+	app.identifier = appObj.identifier;
+
+	//Enabled
+	if(typeof appObj.enabled != 'undefined')
+		app.enabled = appObj.enabled;
+
+	//MaxVersionCode
+	if(typeof appObj.maxVersionCode != 'undefined')
+		app.maxVersionCode = appObj.maxVersionCode;
+
+	//UpdateVersionCode
+	if(typeof appObj.updateVersionCode != 'undefined')
+		app.updateVersionCode = appObj.updateVersionCode;
+
+	//GraceInterval
+	if(typeof appObj.graceInterval != 'undefined')
+		app.graceInterval = appObj.graceInterval;
+
+	//GraceRetrys
+	if(typeof appObj.graceRetrys != 'undefined')
+		app.graceRetrys = appObj.graceRetrys;
+
+	//ValidTime
+	if(typeof appObj.validTime != 'undefined')
+		app.validTime = appObj.validTime;
+
+	//Licenses
+	if(Object.prototype.toString.call(appObj.licenses) != '[object Array]' || appObj.licenses == null){
+		next(new Error ('Missing required parameter appObj.licenses'));
+		return;
+	}
+	if(appObj.licenses.length <= 0){
+		next(new Error('appObj.licenses should have at least 1 license'));
+	}
+	for(var i in appObj.licenses){
+		if(typeof appObj.licenses[i].trialtype == 'undefined' || appObj.licenses[i].trialtype == null){
+			next(new Error ('Missing required parameter appObj.licenses[].trialtype'));
+			return;	
+		}
+	}
+	app.licenses = appObj.licenses;
+
+	//Create the public private key for signin
+	certificateSv.create(appObj.identifier, function(err, pub, priv, cert){
+		if(err){
+			next(err);
+			return;
+		}
+
+		app.publicKey = pub;
+		app.privateKey = priv;
+		app.save(function(err){
+			console.log(typeof err);
+			console.log(err);	
 			if(err){
 				next(err);
 				return;
 			}
-
-			app.publicKey = pub;
-			app.privateKey = priv;
-			app.save(function(err){
-				if(err){
-					next(err);
-					return;
-				}
-				next(null, app);
-			});
-		}); 
-	} else {
-		next(null, null);
-	}
+			next(null, app);
+		});
+	}); 
 }
 
 /* Update a existing @app with data from @newApp
 	 Returns the app wit new data */
 exports.update = function(app, newApp, next){
 	if(!app){
-		next(new Error("Missing app parameter"));
+		next(new Error('Missing app parameter'));
 		return;
 	} 
 
 	if(!newApp){
-		next(new Error("Missing newApp parameter"));
+		next(new Error('Missing newApp parameter'));
 		return;		
 	}
 
@@ -92,7 +142,7 @@ exports.update = function(app, newApp, next){
 /* Deletes a existing @app and returns it */
 exports.delete = function(app, next){
 	if(!app){
-		next(new Error("Missing app parameter"));
+		next(new Error('Missing app parameter'));
 		return;
 	}
 
