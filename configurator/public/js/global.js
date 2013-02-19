a5830d38207d8a3057653b7bff2ea07106441c41
Martin Christmann <martin.christmann@gmail.com>
2013-02-19 23:22:56 +0100

    improve usability

--- configurator/public/js/global.js
+++ configurator/public/js/global.js
@@ -6,4 +6,31 @@ $(function(){
 	$('form[name="setTestResult"] select[name="testResult"]').change(function(){
 		$(this).parent('form').submit();
 	});
+
+	$('form[name="appsettings"] .minus').click(function(){
+		var numberInput = $(this).prevAll('input');
+		var currentNumber = parseInt($(numberInput).val());
+
+		if(currentNumber == NaN)
+			return;
+		if(currentNumber <= 0)
+			return;
+		else 
+			currentNumber--; 
+
+		$(numberInput).val(currentNumber);
+	});
+
+
+	$('form[name="appsettings"] .plus').click(function(){
+		var numberInput = $(this).prevAll('input');
+		var currentNumber = parseInt($(numberInput).val());
+
+		if(currentNumber == NaN)
+			return;
+		else 
+			currentNumber++; 
+
+		$(numberInput).val(currentNumber);
+	});
 });
